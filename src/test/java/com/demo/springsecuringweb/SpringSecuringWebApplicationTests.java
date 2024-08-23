76d00fc31f0b16100ab29568e7712e163c805460
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-08-24 00:34:38 +0800

    initial commit

---
+++ src/test/java/com/demo/springsecuringweb/SpringSecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springsecuringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
